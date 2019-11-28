4278757c1d26b664188c88c4d1f4e2fd9663277c
LouisSoleil <louis.soleilhavoup@hotmail.com>
2019-11-28 12:15:42 +0100

    modif fichier

--- PionIT.swift
+++
@@ -1,8 +0,0 @@
-protocol IteratorPion {
-
-	//permet de créer un itérateur avec tous les pions du joueur rentré en paramètre
-	init (joueur : Int){}
-	
-	//permet de parcourir la liste des pions
-	mutating func next() -> Pion?{}
-}
