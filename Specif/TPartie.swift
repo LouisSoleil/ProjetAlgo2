09b730eeb9705aca98db1894e920076869e6c234
louis.soleilhavoup <louis.soleilhavoup@IGTP4-PC09.isim.intra>
2019-12-11 12:38:36 +0100

    ajout des struct

--- Specif/TPartie.swift
+++ Specif/TPartie.swift
@@ -1,11 +1,28 @@
-protocol Partie {
+struct TPartie {
 
 // Initialise un plateau vide qui est une grille de taille 4x4, ainsi que 2 joueurs et le premier Joueur courant 
 // Aucune case n’est occupée 
 // Chaque position peut prendre soit une pièce soit vide
-	init() {}
+	init(){
+		var pos1 : [[Int](2), Piece?] = [[0,0], Nil]
+		var pos2 : [[Int](2), Piece?] = [[0,1], Nil]
+		var pos3 : [[Int](2), Piece?] = [[0,2], Nil]
+		var pos4 : [[Int](2), Piece?] = [[0,3], Nil]
+		var pos5 : [[Int](2), Piece?] = [[1,0], Nil]
+		var pos6 : [[Int](2), Piece?] = [[1,1], Nil]
+		var pos7 : [[Int](2), Piece?] = [[1,2], Nil]
+		var pos8 : [[Int](2), Piece?] = [[1,3], Nil]
+		var pos9 : [[Int](2), Piece?] = [[2,0], Nil]
+		var pos10 : [[Int](2), Piece?] = [[2,1], Nil]
+		var pos11 : [[Int](2), Piece?] = [[2,2], Nil]
+		var pos12 : [[Int](2), Piece?] = [[2,3], Nil]
+		var pos13 : [[Int](2), Piece?] = [[3,0], Nil]
+		var pos14 : [[Int](2), Piece?] = [[3,1], Nil]
+		var pos15 : [[Int](2), Piece?] = [[3,2], Nil]
+		var pos16 : [[Int](2), Piece?] = [[3,3], Nil]
+		var plateau : [[Int](2), Piece?](16) = [pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9, pos10, pos11, pos12, pos13, pos14, pos15, pos16]
+	}
 
-	
 	var JoueurCourant : Joueur {get}
 // Définit le joueur qui doit poser une pièce
 
